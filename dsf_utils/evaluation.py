eed1adff9c10ac498e9a94acc819fb2a217bc95d
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2022-09-12 22:20:34 -0400

    Evaluation

--- dsf_utils/evaluation.py
+++ dsf_utils/evaluation.py
@@ -92,18 +92,22 @@ def display_results(df, axis=0):
 
     display(results)
 
-
 def evaluate_panel_forecaster_on_cutoffs(
     panel_df: pd.DataFrame,
     cutoffs: list,
     forecaster,
     metric,
-    fh: np.array = np.arange(3) + 1,
-    window_length: int = 5 * 52,
-    freq="W-SUN",
-    ts_id_col="REGION",
-    target="ILITOTAL",
+    fh: np.array, # = np.arange(3) + 1,
+    window_length: int,# = 5 * 52,
+    freq=, # "W-SUN",
+    ts_id_col, # ="REGION",
+    target, # ="ILITOTAL",
 ) -> pd.DataFrame:
+    """ 
+    ts_id_col is the column in panel_df that distinguishes the panels.
+    target is the column to predict
+    
+    """
     _panel_df = panel_df.copy()
     _panel_df = _panel_df.sort_values(by=[ts_id_col]).sort_index()
     ts_list = list(panel_df[ts_id_col].unique())
@@ -121,13 +125,19 @@ def evaluate_panel_forecaster_on_cutoffs(
             (_panel_df.index >= min_test_date) & (_panel_df.index <= max_test_date)
         ]
         # if forecaster doesn't need fh in fit fh will be ignored.
+        # forecaster.fit is not being passed any X, so no support for exogenous variables.
         _forecaster.fit(train_df, fh=fh)
+        # no exog. 
+        # Also, does not take into account panels starting at/after cutoff (=> having no training data => not creating predictions). 
+        
         pred_df = _forecaster.predict(fh=fh)
 
         # loop over regions to get region level metrics and y_preds
         for ts in ts_list:
             _pred = pred_df[pred_df[ts_id_col] == ts]["y_pred"]
             _test = test_df[test_df[ts_id_col] == ts][target]
+            if not (len(_pred) and len(_test)):
+                continue
             _train = train_df[train_df[ts_id_col] == ts][target].sort_index()
             score = metric(y_true=_test, y_pred=_pred, y_train=_train)
             results = results.append(
