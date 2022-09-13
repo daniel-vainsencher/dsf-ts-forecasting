47286e235c991c386055019e570d5958a5707413
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2022-09-13 15:13:31 -0400

    Support X in univariate_to_panel

--- dsf_utils/models/_sktime_univariate_to_panel.py
+++ dsf_utils/models/_sktime_univariate_to_panel.py
@@ -23,24 +23,24 @@ class SktimePanelForecaster:
         ts = ts_df[self.target_col].sort_index()
         return ts
 
-    def fit(self, ts_df: pd.DataFrame, fh=None):
+    def fit(self, ts_df: pd.DataFrame, X: pd.DataFrame=None, fh=None):
         ts_df = ts_df.copy()
         self.is_fitted = False
         for ts_name, ts in ts_df.groupby([self.ts_id_col]):
             ts = self._process_single_ts(ts)
             _forecaster = self.forecaster(**self.forecaster_kwargs)
             if _forecaster._tags["requires-fh-in-fit"]:
-                _forecaster.fit(y=ts, fh=fh)
+                _forecaster.fit(y=ts, fh=fh, X=X)
             else:
-                _forecaster.fit(y=ts)
+                _forecaster.fit(y=ts, X=X)
             self.models_dict[ts_name] = _forecaster
 
         self.is_fitted = True
 
-    def predict(self, fh):
+    def predict(self, fh, X=None):
         pred_df = pd.DataFrame()
         for ts_name, forecaster in self.models_dict.items():
-            y_pred = forecaster.predict(fh).to_frame(name="y_pred")
+            y_pred = forecaster.predict(fh, X=X).to_frame(name="y_pred")
             y_pred[self.ts_id_col] = ts_name
             pred_df = pred_df.append(y_pred, ignore_index=False)
 
